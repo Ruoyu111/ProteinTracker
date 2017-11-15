5a200173778b79dd77caaed09e4cdf31116dd430
RChen <ruoyu.chen@careeverywhere.com>
2017-11-15 13:49:03 -0500

    Hibernate Basic Mapping added Users Entity.

---
+++ src/com/simpleprogrammer/UsersEntity.java
@@ -0,0 +1,64 @@
+package com.simpleprogrammer;
+
+public class UsersEntity {
+    private int id;
+    private String name;
+    private int total;
+    private int goal;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getTotal() {
+        return total;
+    }
+
+    public void setTotal(int total) {
+        this.total = total;
+    }
+
+    public int getGoal() {
+        return goal;
+    }
+
+    public void setGoal(int goal) {
+        this.goal = goal;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+
+        UsersEntity that = (UsersEntity) o;
+
+        if (id != that.id) return false;
+        if (total != that.total) return false;
+        if (goal != that.goal) return false;
+        if (name != null ? !name.equals(that.name) : that.name != null) return false;
+
+        return true;
+    }
+
+    @Override
+    public int hashCode() {
+        int result = id;
+        result = 31 * result + (name != null ? name.hashCode() : 0);
+        result = 31 * result + total;
+        result = 31 * result + goal;
+        return result;
+    }
+}
